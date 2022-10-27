a877937de879c69326349fdb8af5c805192a586d
Mihail Schutow <schutow.mihail@yandex.ru>
2022-10-27 21:53:12 +0300

    fix image changing

--- slider1.js
+++ slider1.js
@@ -1,47 +1,57 @@
-let slider1 = {
-    imagesURLs: [],
-    prevBtn: null,
-    nextBtn: null,
-    sliderImage: null,
-    currentImageIndex: 0,
+function createSlider(){
+    let newSlider = {
+        imagesURLs: [],
+        prevBtn: null,
+        nextBtn: null,
+        sliderImage: null,
+        currentImageIndex: 0,
 
-    start: function (elId) {
+        start: function (elId) {
+            let that = this
 
-        let selector = `#${elId}`;
-        let el = document.querySelector(selector);
+            let selector = `#${elId}`;
+            let el = document.querySelector(selector);
 
-        this.prevBtn = el.querySelector('.prevBtn')
-        this.nextBtn = el.querySelector('.nextBtn')
-        this.sliderImage = el.querySelector('.sliderImage')
+            this.prevBtn = el.querySelector('.prevBtn')
+            this.nextBtn = el.querySelector('.nextBtn')
+            this.sliderImage = el.querySelector('.sliderImage')
 
-        this.imagesURLs.push('https://avatars.mds.yandex.net/i?id=9cdf07fae373c888b11e48e943a1e771-5161032-images-thumbs&n=13&exp=1')
-        this.imagesURLs.push('https://avatars.mds.yandex.net/i?id=03eb9f3bfc40ecb2b07035a769ed9ff7-4825588-images-thumbs&n=13&exp=1')
-        this.imagesURLs.push('https://avatars.mds.yandex.net/i?id=ad59446c5ced921127c88f140d873e4e-4432920-images-thumbs&n=13&exp=1')
-        this.imagesURLs.push('https://avatars.mds.yandex.net/i?id=e056c77877591e07d352d0e86e0118bf-4628413-images-thumbs&n=13&exp=1')
+            this.imagesURLs.push('https://avatars.mds.yandex.net/i?id=9cdf07fae373c888b11e48e943a1e771-5161032-images-thumbs&n=13&exp=1')
+            this.imagesURLs.push('https://avatars.mds.yandex.net/i?id=03eb9f3bfc40ecb2b07035a769ed9ff7-4825588-images-thumbs&n=13&exp=1')
+            this.imagesURLs.push('https://avatars.mds.yandex.net/i?id=ad59446c5ced921127c88f140d873e4e-4432920-images-thumbs&n=13&exp=1')
+            this.imagesURLs.push('https://avatars.mds.yandex.net/i?id=e056c77877591e07d352d0e86e0118bf-4628413-images-thumbs&n=13&exp=1')
 
-        this.prevBtn.disabled = true
-        this.sliderImage.src = this.imagesURLs[this.currentImageIndex]
-
-        this.prevBtn.addEventListener('click', this.onPrevBtnClick.bind(this));
-        this.nextBtn.addEventListener('click', this.onNextBtnClick.bind(this));
-    },
-    onPrevBtnClick: function (event) {
-        this.currentImageIndex--
-        this.nextBtn.disabled = false
-        if (this.currentImageIndex === 0) {
             this.prevBtn.disabled = true
-        }
-        this.sliderImage.src = this.imagesURLs[this.currentImageIndex]
-    },
-    onNextBtnClick: function (event) {
-        this.currentImageIndex++
-        this.prevBtn.disabled = false
-        if (this.currentImageIndex === this.imagesURLs.length - 1) {
-            this.nextBtn.disabled = true
-        }
-        this.sliderImage.src = this.imagesURLs[this.currentImageIndex]
-    },
+            this.sliderImage.src = this.imagesURLs[this.currentImageIndex]
+
+            //way 1
+            this.prevBtn.addEventListener('click', this.onPrevBtnClick.bind(this));
+            //way2
+            this.nextBtn.addEventListener('click', function (e){
+                that.onNextBtnClick(e)
+            });
+        },
+        onPrevBtnClick: function (e) {
+            this.currentImageIndex--
+            this.nextBtn.disabled = false
+            if (this.currentImageIndex === 0) {
+                this.prevBtn.disabled = true
+            }
+            this.sliderImage.src = this.imagesURLs[this.currentImageIndex]
+        },
+        onNextBtnClick: function (e) {
+            this.currentImageIndex++
+            this.prevBtn.disabled = false
+            if (this.currentImageIndex === this.imagesURLs.length - 1) {
+                this.nextBtn.disabled = true
+            }
+            this.sliderImage.src = this.imagesURLs[this.currentImageIndex]
+        },
+    };
+    return newSlider
 };
 
-slider1.start('slider1');
-slider1.start('slider2');
+let slider1 = createSlider();
+slider1.start("slider1");
+let slider2 = createSlider();
+slider2.start('slider2')
